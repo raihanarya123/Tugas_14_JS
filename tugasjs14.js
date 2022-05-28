951d1da69cd2486c4a88f01a08f6d9ba2044aebc
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:11:32 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs14.js
@@ -0,0 +1,9 @@
+function tugas18() {
+    var a = 5;
+    var b = 37 / a;
+    console.log(b);
+    console.log(Math.round(b));
+    console.log(Math.floor(b));
+    console.log(Math.ceil(b)); 
+}
+tugas18();
